14f8072019691e78a253939a1a05648fe1afa628
gradiuscypher <gradiuscypher@gmail.com>
2014-06-16 13:42:58 -0700

    Adding more progress to the DNS server

--- net/dns_server.lua
+++ net/dns_server.lua
@@ -1,16 +1,18 @@
 --DNS Server for Minecraft Network
 event = require("event")
 component = require("component")
+serialization = require("serialization")
 modem = component.modem
-
+book = {}
 modem.open(53)
+dns_book = "/mnt/2dc/code/net/dns_book.txt"
+
 
 event.listen("modem_message", function (_,_,from,port,_,message)
     print(message)
     request, name, address, pin = message:match("([^,]+),([^,]+),([^,]+),([^,]+)")
     
     if request == "lookup" then
-        print("Lookup request:"..from..":"..port)
         lookup_request(from, port, name)
     elseif request == "register" then
         print("Register request:"..from..":"..port)
@@ -21,8 +23,21 @@ event.listen("modem_message", function (_,_,from,port,_,message)
     end
 end)
 
+function load_book_from_file ()
+    local file = io.open(dns_book, "r")
+    local read_file = io.read("*all")
+    return serialization.unseralize(read_file)
+end
+
+function save_book_to_file (book_obj)
+    local file_write = assert(io.open(dns_book, "w"))
+    out_str = serialization.serialize(book_obj)
+    file_write:write(out_str)
+    file_write:close()
+end
+
 function lookup_request (from, port, name)
-    print("Got request to look up "..name)
+    print("[LOOKUP] "..from..":"..port.." - "..name)
     modem.send(from, port, "TEST-ADDRESS123")
 end
 
